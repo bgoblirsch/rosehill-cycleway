f151d8b578d1cd3e5662c74a6490846404b10cf3
Brandon <21012176+bgoblirsch@users.noreply.github.com>
2023-09-27 11:37:25 -0500

    removed extra word

--- src/components/StoryPane/MoreInfo.tsx
+++ src/components/StoryPane/MoreInfo.tsx
@@ -20,7 +20,7 @@ const MoreInfo: React.FC = () => {
         <p>Please make sure to check out the other projects once they have been posted! To stay up to date, the 40th ward's blog can be found at: <StoryLink to="https://40thward.org/blog/" text="https://40thward.org/blog/"></StoryLink></p>
         <p>For more information on Participatory Budgeting and the process, see the 40th ward's website: <StoryLink to="https://40thward.org/topics-and-services/participatory-budgeting/" text="https://40thward.org/topics-and-services/participatory-budgeting/"></StoryLink></p>
         <p>If you have any feedback or questions about this project proposal or the website, I'd love to hear from you. I would also appreciate reports for any data inaccuracies. You may use this <StoryLink to="https://forms.gle/vrPuhXooxCL5Tvee6" text="Google form"></StoryLink> to get in touch.</p>
-        <Disclaimer>The data for the ward boundaries and existing bike routes were sourced from the <StoryLink to="https://data.cityofchicago.org/" text="Chicago Data Portal"></StoryLink> and the existing bike route data received a few edits to better better reflect the actual bike network. All other data was manually digitized for the purpose of this proposal.</Disclaimer>
+        <Disclaimer>The data for the ward boundaries and existing bike routes were sourced from the <StoryLink to="https://data.cityofchicago.org/" text="Chicago Data Portal"></StoryLink> and the existing bike route data received a few edits to better reflect the actual bike network. All other data was manually digitized for the purpose of this proposal.</Disclaimer>
         <Disclaimer><Link target="_blank" to="/privacy-policy" style={{ color: 'white' }}>Privacy Policy</Link></Disclaimer>
 
         <p>&nbsp;</p>
